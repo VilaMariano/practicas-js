edc49cd8551703ff094f96a4f865356a02e7ab77
VilaMariano <vilam@udesa.edu.ar>
2024-05-19 12:06:12 -0300

    practitas js

---
+++ js/experimento.js
@@ -0,0 +1,7 @@
+let nombre = prompt("Cual es tu nombre?");
+console.log(nombre);
+let edad = prompt("Cuantos años tenes?");
+console.log(edad);
+let fanDeportes = confirm("Te gustan los deportes?");
+alert(`Muchas gracias ${nombre} por responder nuestras preguntas”`);
+usuario = [];
